12f81b33e53980f6a4c2b646f6a24eb6641541d7
FlexaDela <mateusflexa2000@gmail.com>
2025-01-13 20:44:35 -0300

    inserindo rotas dentro do router

---
+++ api/src/controllers/usuarios.go
@@ -0,0 +1,23 @@
+package controllers
+
+import "net/http"
+
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando usuario"))
+}
+
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando usuario"))
+}
+
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando usuario"))
+}
+
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando usuario"))
+}
+
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Criando usuario"))
+}
