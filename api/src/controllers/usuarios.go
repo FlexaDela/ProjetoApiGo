938d260c0ee8ee4ed26faff7b4bb359e1554d2d1
Matheus Flexa <mateusflexa2000@gmail.com>
2025-01-14 19:42:16 -0300

    config

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -7,17 +7,17 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 }
 
 func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuario"))
+	w.Write([]byte("Buscando usuario"))
 }
 
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuario"))
+	w.Write([]byte("Buscando usuarios"))
 }
 
 func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuario"))
+	w.Write([]byte("Atualizando usuario"))
 }
 
 func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Criando usuario"))
+	w.Write([]byte("Deletando usuario"))
 }
