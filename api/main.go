0fcdc5eb4dff61a118e21751ea7b49651b30ceef
FlexaDela <mateusflexa2000@gmail.com>
2025-01-14 20:03:44 -0300

    conectando como banco

--- api/main.go
+++ api/main.go
@@ -10,10 +10,8 @@ import (
 
 func main() {
 	config.Carregar()
-	fmt.Println(config.StringConexaoBanco)
-	fmt.Println("RodandoAPI")
-
 	r := router.Gerar()
 
+	fmt.Printf("Escutando na porta %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
