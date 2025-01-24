fb2bfd049f27de9c107aeb9a47e6bded0fb41ed7
FlexaDela <mateusflexa2000@gmail.com>
2025-01-24 18:31:47 -0300

    usando hash[

--- api/src/modelos/Usuarios.go
+++ api/src/modelos/Usuarios.go
@@ -1,6 +1,7 @@
 package modelos
 
 import (
+	"api/src/seguranca"
 	"errors"
 	"strings"
 	"time"
@@ -22,9 +23,11 @@ func (usuario *Usuario) Preparar(etapa string) error {
 		return erro
 	}
 
-	usuario.formatar()
-	return nil
+	if erro := usuario.formatar(etapa); erro != nil {
+		return erro
+	}
 
+	return nil
 }
 
 func (usuario *Usuario) validar(etapa string) error {
@@ -51,8 +54,19 @@ func (usuario *Usuario) validar(etapa string) error {
 	return nil
 }
 
-func (usuario *Usuario) formatar() {
+func (usuario *Usuario) formatar(etapa string) error {
 	usuario.Nome = strings.TrimSpace(usuario.Nome)
 	usuario.Nick = strings.TrimSpace(usuario.Nick)
 	usuario.Email = strings.TrimSpace(usuario.Email)
+
+	if etapa == "cadastro" {
+		senhaComHash, erro := seguranca.Hash(usuario.Senha)
+		if erro != nil {
+			return erro
+		}
+
+		usuario.Senha = string(senhaComHash)
+	}
+
+	return nil
 }
