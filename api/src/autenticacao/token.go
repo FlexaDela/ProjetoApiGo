08e4b09d4692399ec9d5f06bd4e3ac60388059ce
FlexaDela <mateusflexa2000@gmail.com>
2025-02-06 16:59:30 -0300

    extrait usuadio id do token

--- api/src/autenticacao/token.go
+++ api/src/autenticacao/token.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 
@@ -36,6 +37,26 @@ func ValidarToken(r *http.Request) error {
 	return errors.New("token invalido")
 }
 
+// retornar o usuarioID que está salvo no token
+func ExtrairUsuarioID(r *http.Request) (uint64, error) {
+	tokenString := extrairToken(r)
+	token, erro := jwt.Parse(tokenString, retornarChaveDeVerificacao)
+	if erro != nil {
+		return 0, erro
+	}
+
+	if permissoes, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		usuarioID, erro := strconv.ParseUint(fmt.Sprintf("%.0f", permissoes["usuarioId"]), 10, 64)
+		if erro != nil {
+			return 0, erro
+		}
+
+		return usuarioID, nil
+	}
+
+	return 0, errors.New("token inválido")
+}
+
 func extrairToken(r *http.Request) string {
 	token := r.Header.Get("Authorization")
 
@@ -48,7 +69,7 @@ func extrairToken(r *http.Request) string {
 
 func retornarChaveDeVerificacao(token *jwt.Token) (interface{}, error) {
 	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-		return nil, fmt.Errorf("Método de assinatura inesperado! %v", token.Header["alg"])
+		return nil, fmt.Errorf("método de assinatura inesperado %v", token.Header["alg"])
 	}
 
 	return config.SecretKey, nil
