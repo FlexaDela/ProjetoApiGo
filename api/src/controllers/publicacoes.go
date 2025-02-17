97e2272f5b564454ca04d7d72bae7eb21f793608
FlexaDela <mateusflexa2000@gmail.com>
2025-02-17 11:57:42 -0300

    esta buscando apenas uma publicacao

--- api/src/controllers/publicacoes.go
+++ api/src/controllers/publicacoes.go
@@ -9,6 +9,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
@@ -57,7 +60,28 @@ func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
 
 }
 func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+	parametros := mux.Vars(r)
+	publicacaoID, erro := strconv.ParseUint(parametros["publicacaoID"], 10, 64)
+	if erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
 
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorio.NovoRepositorioDePublicacoes(db)
+	publicacao, erro := repositorio.BuscarPorID(publicacaoID)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, publicacao)
 }
 func AtualizarPublicas(w http.ResponseWriter, r *http.Request) {
 
