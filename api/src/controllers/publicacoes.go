bbd3e7277cd253e7784f6572fbc82e0e6da630a4
FlexaDela <mateusflexa2000@gmail.com>
2025-02-17 11:18:18 -0300

    criação de publicacao

---
+++ api/src/controllers/publicacoes.go
@@ -0,0 +1,62 @@
+package controllers
+
+import (
+	"api/src/autenticacao"
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorio"
+	"api/src/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func CriarPublicacao(w http.ResponseWriter, r *http.Request) {
+	usuarioID, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	corpoRequisicao, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publicacao modelos.Publicacao
+	if erro = json.Unmarshal(corpoRequisicao, &publicacao); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publicacao.AutorID = usuarioID
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorio.NovoRepositorioDePublicacoes(db)
+	publicacao.ID, erro = repositorio.Criar(publicacao)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusCreated, publicacao)
+}
+func BuscarPublicacoes(w http.ResponseWriter, r *http.Request) {
+
+}
+func BuscarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
+func AtualizarPublicas(w http.ResponseWriter, r *http.Request) {
+
+}
+func DeletarPublicacao(w http.ResponseWriter, r *http.Request) {
+
+}
